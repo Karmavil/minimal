e5505c96dc7c8a40039fab7ba97797eb043050ff
Karmavil <musicapidae@gmail.com>
2022-03-13 14:23:40 -0300

    Add class that inherit from RefCounted Fix warning/error thrown by the editor for library not found removing bin from the path (It tries to find the library in res:/bin/bin while the file is already in res:/bin) Add missing godot-cpp include in CMakeLists.txt

--- include/bug.hpp
+++ include/bug.hpp
@@ -7,9 +7,26 @@
 #include <node.hpp>
 #include <timer.hpp>
 #include <utility_functions.hpp>
+#include <ref.hpp>
+#include <variant.hpp>
 
 namespace minimal
 {
+
+    class BugRef : public godot::RefCounted
+    {
+        GDCLASS(BugRef, godot::RefCounted);
+
+      protected:
+        static void _bind_methods()
+        {
+        }
+
+      public:
+        BugRef();
+        ~BugRef();
+    };
+
     class Bug : public godot::Node
     {
         GDCLASS(Bug, godot::Node);
@@ -24,7 +41,11 @@ namespace minimal
         Bug();
         ~Bug();
         void nextSlide();
-        virtual void _ready();
+        virtual void _ready() override;
+
+        minimal::BugRef *return_extended_ref() const;
+        godot::Ref<minimal::BugRef> extended_ref_checks(godot::Ref<minimal::BugRef> p_ref) const;
+        godot::Variant varargs_func(const godot::Variant **args, GDNativeInt arg_count, GDNativeCallError &error);
     };
 
 } // namespace minimal
